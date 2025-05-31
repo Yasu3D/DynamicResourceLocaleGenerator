4d8423b689e4f20d3cd6cd89b01d80ebaf30eecc
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-06-01 00:54:05 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Csv;
+
+namespace DynamicResourceLocaleGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "locales.csv");
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("No locales found. Press any key to exit.");
+                Console.ReadKey();
+                return;
+            }
+            
+            string csv = File.ReadAllText("locales.csv");
+            IEnumerable<ICsvLine> lines = CsvReader.ReadFromText(csv);
+
+            foreach (ICsvLine line in lines)
+            {
+                Console.WriteLine(line.Raw);
+            }
+        }
+    }
+}
